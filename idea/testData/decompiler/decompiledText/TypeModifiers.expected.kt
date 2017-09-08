248d01a515a0235a8349b6c271eaf640554d180a
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2017-09-08 19:04:31 +0300

    Render suspend and annotations in same modifier list

--- idea/testData/decompiler/decompiledText/TypeModifiers.expected.kt
+++ idea/testData/decompiler/decompiledText/TypeModifiers.expected.kt
@@ -8,7 +8,7 @@ public final class TypeModifiers public constructor() {
 
     public final val functionOnSuspendFunction: (suspend () -> kotlin.Unit).() -> kotlin.Unit /* compiled code */
 
-    public final val suspendExtFunction: suspend kotlin.Any.() -> kotlin.Unit /* compiled code */
+    public final val suspendExtFunction: (suspend kotlin.Any.() -> kotlin.Unit) /* compiled code */
 
     public final val suspendFunction: suspend () -> kotlin.Unit /* compiled code */
 }
--- idea/testData/decompiler/decompiledText/TypeModifiers/TypeModifiers.kt
+++ idea/testData/decompiler/decompiledText/TypeModifiers/TypeModifiers.kt
@@ -5,7 +5,7 @@ class TypeModifiers {
 
     val suspendFunction: suspend () -> Unit = null!!
 
-    val suspendExtFunction: suspend Any.() -> Unit = null!!
+    val suspendExtFunction: (suspend Any.() -> Unit) = null!!
 
     val functionOnSuspendFunction: (suspend () -> Unit).() -> Unit = null!!
 }
